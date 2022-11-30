0a8203e95512f9ae394f00c7afeaa7b73eac17e3
Charles Clark <Charles.Clark@stonybrook.edu>
2022-11-30 14:01:07 -0500

    replaced for loop with np casting

--- test_operations.py
+++ test_operations.py
@@ -88,12 +88,6 @@ def outlier_rejection(*dfs, data_window=60, threshold=25):
         np.sum(((all_data - trim_mean) / trim_std) ** 2, axis=1) / n_pts
     )
     print(deviation_from_mean**0.5)
-    for df in dfs:
-        data = df["mu_trans"]
-        deviation_from_mean = (
-            1 / n_pts * np.sum(((data - trim_mean) / trim_std) ** 2)
-        )
-        print(deviation_from_mean**0.5)
 
 
 def test_outlier():
